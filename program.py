2278e9b2290ffec008b78602ca1e0d492be393ea
Jack Kinsella <151206+jackkinsella@users.noreply.github.com>
2019-03-17 15:40:50 +0100

    Accept command line arg for midi location

--- program.py
+++ program.py
@@ -1,19 +1,11 @@
 import mido
+import sys
 import os
 
-midifile_path = 'midis/sanctus.mid'
-description = {
-  "soprano": [0,1,2],
-  "alt": [0,1,3],
-  "tenor": [0,1,4],
-  "bass": [0,1,5]
-}
-midi_data = mido.MidiFile(midifile_path)
+def track_names(midi_data):
+  return list(map(lambda track: track.name, midi_dat.tracks))
 
-def track_names():
-  return list(map(lambda track: track.name, midi_data.tracks))
-
-def isolate_tracks(track_numbers, output_filename):
+def isolate_tracks(midi_data, track_numbers, output_filename):
   new_file = mido.MidiFile()
 
   for track_number in track_numbers:
@@ -30,15 +22,28 @@ def convert_midi_to_mp3(midifile_path):
     midifile_path, file_name_without_extension)
   )
 
-def change_instrument(program_number):
+def change_instrument(midi_data, program_number):
   for track in midi_data.tracks:
     for message in track:
       if message.type == 'program_change':
         message.program = program_number
 
-def separate_tracks_into_mp3s(description):
+def separate_tracks_into_mp3s(midi_data, description):
   for track_name, track_numbers in description.items():
-    convert_midi_to_mp3(isolate_tracks(track_numbers, track_name))
+    convert_midi_to_mp3(isolate_tracks(midi_data, track_numbers, track_name))
 
-change_instrument(0)
-separate_tracks_into_mp3s(description)
+
+midifile_path = sys.argv[1:]
+
+# TODO: Document this and make optional
+description = {
+  "soprano": [0,1,2],
+  "alt": [0,1,3],
+  "tenor": [0,1,4],
+  "bass": [0,1,5]
+}
+
+midi_data = mido.MidiFile(midifile_path)
+
+change_instrument(midi_data, 0)
+separate_tracks_into_mp3s(midi_data, description)
