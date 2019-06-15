88a1dbdc785de9ea91bc7eb53e6b905080fdfe79
Jack Kinsella <151206+jackkinsella@users.noreply.github.com>
2019-06-15 20:12:54 +0200

    Switch from German to English in file names

--- tests/test-integration.py
+++ tests/test-integration.py
@@ -9,12 +9,11 @@ class TestIntegration(unittest.TestCase):
         shutil.rmtree(output_directory)
 
     def test_conversion_of_midi(self):
-        # TODO: Clean up between tests
         midi_file_path = "./tests/fixtures/Schubert-872-sanctus.mid"
         cmd = "./midi-to-part-mp3s {} -s 0 -a 1 -t 2 -b 3".format(midi_file_path)
         os.system(cmd)
 
-        all_parts_mp3_output = "./output/alle.mp3"
+        all_parts_mp3_output = "./output/all.mp3"
         self.assertTrue(os.path.isfile(all_parts_mp3_output))
 
 if __name__ == '__main__':
