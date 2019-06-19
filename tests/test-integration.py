fae0a85b1f43dae301306d3be9e1e11a52005155
Jack Kinsella <151206+jackkinsella@users.noreply.github.com>
2019-06-19 10:12:23 +0200

    Add regression test for audio sync issue

--- tests/test-integration.py
+++ tests/test-integration.py
@@ -2,19 +2,36 @@ import unittest
 import os
 import shutil
 
+import sox # type: ignore
+
 output_directory = "./output"
+
+def audio_length(mp3_filepath):
+    return sox.file_info.duration(mp3_filepath)
 
 class TestIntegration(unittest.TestCase):
     def setUp(self):
         shutil.rmtree(output_directory)
 
-    def test_conversion_of_midi(self):
-        midi_file_path = "./tests/fixtures/Schubert-872-sanctus.mid"
-        cmd = "./midi-to-part-mp3s {} -s 0 -a 1 -t 2 -b 3".format(midi_file_path)
+    def test_conversion_of_midi_with_separate_tempo_map(self):
+        midi_file_path = "./tests/fixtures/Schumann-op67-4.mid"
+        cmd = "./midi-to-part-mp3s {}".format(midi_file_path)
         os.system(cmd)
 
-        all_parts_mp3_output = "./output/all.mp3"
+        all_parts_mp3_output = "{}/all.mp3".format(output_directory)
         self.assertTrue(os.path.isfile(all_parts_mp3_output))
+
+    def test_midi_without_separate_tempo_map(self):
+        midi_file_path = "./tests/fixtures/Brahms-Da-unten-im-Tale.mid"
+        cmd = "./midi-to-part-mp3s {}".format(midi_file_path)
+        os.system(cmd)
+
+        audio_lengths = []
+        for output_file in ['soprano', 'alto', 'bass', 'tenor']:
+            audio_lengths.append(audio_length("{}/{}.mp3".format(output_directory,
+                output_file, ".mp3")))
+
+        self.assertSequenceEqual(audio_lengths, [audio_lengths[0]] * 4)
 
 if __name__ == '__main__':
     unittest.main()
