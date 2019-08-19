02d7e09a565be64ec838196ae8b090fe9f18b1f4
Jack Kinsella <151206+jackkinsella@users.noreply.github.com>
2019-08-19 19:12:27 +0200

    Add default_config to separate file

---
+++ default_config.py
@@ -0,0 +1,14 @@
+from custom_types import ConfigType
+
+default_config: ConfigType = {
+    "alto": [2],
+    "bass": [4],
+    "file_path": '',
+    "instrument": 49,
+    "instrumental_accompaniment": [],
+    "instrumental_volume": 2.0,
+    "output_directory": "./output",
+    "soprano": [1],
+    "soundfont_path": "./soundfonts/timbres-of-heaven.sf2",
+    "tenor": [3]
+}
