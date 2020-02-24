ed2472f2f9185b36acb0fe23b30a7b9c8a94ebbd
Jack Kinsella <151206+jackkinsella@users.noreply.github.com>
2020-02-24 11:22:52 +0700

    Expand debug functionality

--- midi_to_part_mp3s/analysis.py
+++ midi_to_part_mp3s/analysis.py
@@ -1,7 +1,8 @@
 import mido
 
 
-def analyze(mido: mido.MidiFile):
+# FIXME: Add a command line message for log_all_messages
+def analyze(mido: mido.MidiFile, log_all_messages=False):
     print("Analyzing midi file\n")
     num_tracks = len(mido.tracks)
     print(f"Number of tracks: {num_tracks}\n")
@@ -12,3 +13,6 @@ def analyze(mido: mido.MidiFile):
             track_name = f"Track {index}"
         message_count = track.__len__()
         print(f" >Track {index}: {track_name}: {message_count} messages")
+        if log_all_messages:
+            for message in track:
+                print(message)
