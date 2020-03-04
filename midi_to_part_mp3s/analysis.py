581ff42f41c2cf343fc54b0566937c637bbb8a0e
Jack Kinsella <151206+jackkinsella@users.noreply.github.com>
2020-03-04 11:41:16 +0700

    Fix bugs with analysis

--- midi_to_part_mp3s/analysis.py
+++ midi_to_part_mp3s/analysis.py
@@ -1,22 +1,41 @@
 import mido
 
 
-def analyze(mido: mido.MidiFile, log_all_midi_messages=False):
+def analyze(midi: mido.MidiFile, log_all_midi_messages=False):
     print("Analyzing midi file\n")
-    num_tracks = len(mido.tracks)
-    print(f"Number of tracks: {num_tracks}\n")
-    for index, track in enumerate(mido.tracks):
+    print(f"Number of tracks: {number_of_tracks(midi)}\n")
+    if is_split_by_channel(midi):
+        print("Split by channel\n")
+    else:
+        print("Split by track\n")
+    for index, track in enumerate(midi.tracks):
         if hasattr(track, "name"):
             track_name = track.name
         else:
             track_name = f"Track {index}"
-        number_of_midi_channels = len(
-            set(message.channel for message in track if not message.is_meta)
-        )
         message_count = track.__len__()
-        print(f"> Track {index}: {track_name}: {number_of_midi_channels}"
+        print(f"> Track {index}: {track_name}:"
+              f" {number_of_midi_channels(track)}"
               f" channels {message_count} messages")
         if log_all_midi_messages:
             print("\n\n")
             for message in track:
                 print(message)
+
+
+def number_of_midi_channels(track: mido.MidiTrack):
+    return len(
+        set(message.channel for message in track if not message.is_meta)
+    )
+
+
+def number_of_tracks(midi: mido.MidiFile):
+    return len(midi.tracks)
+
+
+def is_split_by_channel(midi: mido.MidiFile):
+    last_track = midi.tracks[-1]
+    if number_of_tracks(midi) <= 2 and number_of_midi_channels(last_track) > 1:
+        return True
+    else:
+        return False
